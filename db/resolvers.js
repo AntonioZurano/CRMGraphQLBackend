81f716a388e6731c402b9314bc0766284d8caeb4
Antonio Zurano Blazquez <azurano@outlook.es>
2023-06-30 11:50:24 +0200

    insertando usuarios en la BBDD

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,10 +1,30 @@
+const Usuario = require('../models/Usuario');
 
-//Resolvers
 const resolvers = {
-    Query: {
-        obtenerCursos: () => 'Algo'
-        
+  Query: {
+    obtenerCursos: () => 'Algo'
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error('El usuario ya está registrado');
+      }
+
+      try {
+        const usuario = new Usuario(input);
+        usuario.save();
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
     }
-}
+  }
+};
 
 module.exports = resolvers;
+
+
+
--- db/schema.js
+++ db/schema.js
@@ -1,27 +1,29 @@
 const { gql } = require('apollo-server');
 
-
-
-
-
-//Schema
 const typeDefs = gql`
     type Usuario {
+        id: ID
         nombre: String
         apellido: String
         email: String
         password: String
         creado: String
     }
-    
-    type Query {
-        obtenerCurso: String
+
+    input UsuarioInput {
+        nombre: String!
+        apellido: String!
+        email: String!
+        password: String!
     }
-    
+
+    type Query {
+        obtenerCursos: String
+    }
+
     type Mutation {
-        nuevoUsuario: Usuario        
+        nuevoUsuario(input: UsuarioInput): Usuario  
     }
 `;
-
 
 module.exports = typeDefs;
